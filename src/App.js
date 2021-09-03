8221eccbb3db20cca3c4bb6729559990419701e4
pixelsGovernor <pixelsGovernor@gmail.com>
2021-09-03 09:06:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css'
+import Form from './widgets/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/widgets/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Input } from 'antd';
+
+Form.propTypes = {
+
+};
+
+function Form( props ) {
+  return (
+    <div>
+      <Input placeholder="Basic usage" />
+    </div>
+  );
+}
+
+export default Form;
